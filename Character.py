490b06cc1e40979931d0d2fe24c3b1b7bcb59912
stiffchoclate <abs.ismail08@gmail.com>
2024-09-13 12:20:37 +0100

    Started writing character class

---
+++ Character.py
@@ -0,0 +1,9 @@
+import pygame
+
+#'Characters' represnt the sprite that players can control.
+class Character(pygame.sprite.Sprite):
+    def __init__(self, center, x, y, image):
+        pygame.sprite.Sprite()
+        self.center = (x,y)
+        self.x
+        self.y
